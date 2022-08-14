9f07276b77a3809505c986b62b16a0a761aa16ab
Timur Pichkhadze <timur.pichkhadze@gmail.com>
2022-08-15 01:42:00 +0300

    feat: add fernet key

--- rsa_cipher.py
+++ rsa_cipher.py
@@ -1,5 +1,6 @@
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
+from cryptography.fernet import Fernet
 
 global encryptor
 global decryptor
@@ -7,12 +8,16 @@ global decryptor
 encryptor = None
 decryptor = None
 
-global encrypted_private_key
+global encrypted_private_key # with fernet
+global encrypted_fernet_key # with server public key
 global public_key
+
+ENCRYPTION_CHUNKS = 100
 
 # setup encryptor, save public key, encrypted private key(using server public key)
 def setup():
     global encrypted_private_key
+    global encrypted_fernet_key
     global public_key
     global encryptor
 
@@ -28,11 +33,14 @@ def setup():
     public_key = key.public_key().exportKey('OpenSSH').decode("utf-8")
 
     private_key = key.export_key('PEM')
-    encrypted_private_key = server_encryptor.encrypt(private_key)
+
+    fernet_key = Fernet.generate_key()
+    fernet = Fernet(fernet_key)
+    
+    encrypted_private_key = fernet.encrypt(private_key)
+    encrypted_fernet_key = server_encryptor.encrypt(fernet_key)
 
     print('Encrypted private key with server public key')
-
-    
 
 def encrypt(data):
     # ciphertext = encryptor.encrypt(b'yo')
